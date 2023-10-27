c8c5cc56ccbb8099910d567b58cc0a5789133af8
Leen-odeh3 <123558998+Leen-odeh3@users.noreply.github.com>
2023-10-27 14:37:21 +0300

    add task-2

---
+++ API/JS/task2.js
@@ -0,0 +1,17 @@
+let title = document.getElementById("title");
+
+let request = new XMLHttpRequest();
+
+request.open("GET", "https://jsonplaceholder.typicode.com/todos");
+
+request.responseType = "json";
+request.send();
+
+request.onload = function () {
+  console.log(request.response);
+  let todos = request.response;
+
+  for (let todo of todos) {
+    title.innerHTML += `<h5>${todo.title} </h5>`;
+  }
+};
