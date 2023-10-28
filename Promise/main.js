27950fc87f958b842351c651033aa5134d125444
Leen-odeh3 <123558998+Leen-odeh3@users.noreply.github.com>
2023-10-28 13:35:24 +0300

    Add files via upload

---
+++ Promise/main.js
@@ -0,0 +1,48 @@
+//callback Hell
+
+// setTimeout(() => {
+//     document.getElementById("one").style.visibility="visible"
+//     setTimeout(() => {
+//         document.getElementById("two").style.visibility="visible"
+//         setTimeout(() => {
+//             document.getElementById("three").style.visibility="visible"
+//             setTimeout(() => {
+//                 document.getElementById("four").style.visibility="visible"
+//             }, 1000);
+//         }, 1000);
+//     }, 1000);
+// }, 1000);
+
+//using promise => promises Chaining
+
+let p = new Promise(function (resolve, reject) {
+  setTimeout(() => {
+    document.getElementById("one").style.visibility = "visible";
+    resolve();
+  }, 1000);
+})
+  .then(() => {
+    return new Promise(function (resolve, reject) {
+      setTimeout(() => {
+        document.getElementById("two").style.visibility = "visible";
+        resolve();
+      }, 1000);
+    });
+  })
+  .then(() => {
+    return new Promise(function (resolve, reject) {
+      setTimeout(() => {
+        document.getElementById("three").style.visibility = "visible";
+        resolve();
+      }, 1000);
+    });
+  })
+  .then(() => {
+    setTimeout(() => {
+      document.getElementById("four").style.visibility = "visible";
+      resolve();
+    }, 1000);
+  });
+
+
+
